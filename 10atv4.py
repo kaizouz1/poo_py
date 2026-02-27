4249aa88502dae613c9e8cae04b65e5750162969
Kaizouz1 <joaoleiteferraco@gmail.com>
2026-02-27 14:30:16 -0300

    Add files via upload

--- 10atv4.py
+++ 10atv4.py
@@ -9,4 +9,22 @@ class Carro:
     self.velocidade = velocidade
 
  def acelerar(self, valor):
+    self.aceleracao = valor
+    print(f"Uno de escada acelerou {self.aceleracao}Km")
     
+
+ def freiar(self, freiou):
+     self.freio = self.aceleracao - freiou
+     print(f"uno de escada freiou {self.freio}km")
+
+ def status(self):
+    self.veloz = (self.velocidade + self.aceleracao) - self.freio
+    print(f"Modelo: {self.modelo}\nVelocidade: {self.veloz:.2f}")
+    
+        
+
+carro1 = Carro("Uno de escada", 0)
+carro1.acelerar(100)
+carro1.freiar(50)
+carro1.status()
+
