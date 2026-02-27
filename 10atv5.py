0df61c392a92761ab52effb5c90887e2b68ff030
Kaizouz1 <joaoleiteferraco@gmail.com>
2026-02-27 15:13:30 -0300

    finalizado

--- 10atv5.py
+++ 10atv5.py
@@ -3,6 +3,25 @@ def limpar():
     os.system("cls")
 limpar()
 
-#tesstee
 
-""
+class Livro:
+
+ def __init__(self, titulo, autor, paginas):
+   self.titulo = titulo
+   self.autor = autor
+   self.paginas = paginas
+
+ def detalhes(self):
+   print(f"Título: {self.titulo}\nAutor:{self.autor}\nPáginas")
+
+ def longo(self):
+   if self.paginas > 300:
+     print(f"{self.titulo} possui mais de 300 páginas!")
+     return True
+   else:
+     print(f"{self.titulo} não possui mais de 300 páginas")
+     return False
+   
+livro1 = Livro("Boruto Two Blue Vortex", "kaizou", 184)
+livro1.detalhes()
+livro1.longo()
