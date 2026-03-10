b2ea683daeb27c55f705a3e1faa9a86d16fcb2ac
kaizouz1 <joaoleiteferraco@gmail.com>
2026-03-10 16:57:43 -0300

    finalizado

--- aleatorio.py
+++ aleatorio.py
@@ -3,9 +3,25 @@ def limpar():
     os.system("cls")
 limpar()
 
-senha = print(input("digite a senha: "))
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome 
+        self.idade = idade
+        
+    def apresentar(self):
+        print(f"Olá, meu nome é {self.nome}")
+    
+class Aluno(Pessoa):
+    def estudar(self):
+        print(f"{self.nome} esta estudando")
+        
+class Professor(Pessoa):
+    
+    def ensinar(self):
+        print(f"{self.nome} esta ensinando")
 
-if senha != "1234":
-    print("Acesso concedido!")
-else:
-    print("Acesso negado!")
+a = Aluno("Kaizou", 20)
+p = Professor("Ana", 35)
+
+a.apresentar()
+a.estudar()
